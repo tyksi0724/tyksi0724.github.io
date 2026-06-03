6658a193b5ddc65dba1a94b09d802e274c778a7a
tyksi0724 <85172981+tyksi0724@users.noreply.github.com>
2026-06-03 16:01:29 +0900

    Commit 2.1

--- js/works.js
+++
@@ -1,8 +0,0 @@
-const works = [
-    {
-        title: "ネオネオン icebo Garage Bootleg",
-        description: "ネオネオン / Demo*27のGarageを作りました．Free dl対応",
-        image: "assets/works/work1.jpg",
-        link: "https://x.com/icebodayo/status/2059945277658906883?s=20",
-    },
-];
